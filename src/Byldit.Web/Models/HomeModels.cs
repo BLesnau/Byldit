a345fe964ad5306bbb94dc99a35b0292b20529f0
BLesnau <lesnaubr@gmail.com>
2013-05-16 03:45:03 -0400

    Initial Layout of Laning Page\n\nNo Functionality

---
+++ src/Byldit.Web/Models/HomeModels.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Byldit.Web.Models
+{
+   public class EmailModel
+   {
+      [Required]
+      [Display( Name = "Name" )]
+      public string Name { get; set; }
+
+      [Required]
+      [Display( Name = "Email" )]
+      public string Email { get; set; }
+   }
+}
