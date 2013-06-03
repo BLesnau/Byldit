7b909054791f0b6bc1f6626d112e7b4fb53f3439
BLesnau <lesnaubr@gmail.com>
2013-06-03 01:09:50 -0400

    Renamed Projects / Ported Pages / Staging Setup

---
+++ src/Byldit.Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+using Byldit.DataModel;
+using Byldit.Web.Configs;
+using Microsoft.WindowsAzure.Storage;
+using TechSmith.Hyde;
+using TechSmith.Hyde.Table;
+
+namespace Byldit.Web.Controllers
+{
+   public class HomeController : Controller
+   {
+      public ActionResult Index()
+      {
+         return View();
+      }
+
+      [HttpPost]
+      public ActionResult Index( SubmitEmail model )
+      {
+         if ( ModelState.IsValid )
+         {
+            var connectionString = GlobalConfig.GetString( "BylditStorageAccountConnectionString" );
+            var storageAccount = CloudStorageAccount.Parse( connectionString );
+            var tableStorageProvider = new AzureTableStorageProvider( new CloudStorageAccountAdapter( storageAccount ) );
+            var emailTableName = GlobalConfig.GetString( "SubmitEmailTableName" );
+
+            storageAccount.CreateCloudTableClient().GetTableReference( emailTableName ).CreateIfNotExists();
+
+            tableStorageProvider.Upsert( emailTableName, model );
+            tableStorageProvider.Save();
+
+            model.Submitted = true;
+         }
+
+         return View( model );
+      }
+
+      public ActionResult Beta()
+      {
+         ViewBag.AzureMobileServiceUrl = GlobalConfig.GetString( "AzureMobileServiceUrl" );
+         ViewBag.AzureMobileServiceKey = GlobalConfig.GetString( "AzureMobileServiceKey" );
+
+         return View();
+      }
+   }
+}
