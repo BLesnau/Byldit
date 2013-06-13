ae0a211e8a73c8bb11014e410a476e36ed41d239
BLesnau <lesnaubr@gmail.com>
2013-06-13 02:15:35 -0400

    Added Places search box

---
+++ src/Byldit.Web/Scripts/SearchBox.js
@@ -0,0 +1,14 @@
+﻿function SearchBox(controlDiv) {
+
+    var box = document.createElement("input");
+    box.type = "text";
+    box.className = "searchBox";
+    box.id = "searchBox";
+    //showTagsImage.style.cursor = 'pointer';
+    //showTagsImage.style.marginRight = "10px";
+
+    controlDiv.appendChild(box);
+    controlDiv.index = -1;
+
+    return box;
+}
